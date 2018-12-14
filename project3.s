4e95722bfe454f036d04efc4e703690c368d0a20
Raymond Papenfuss Gyabeng <39167251+Rhaypapenfuzz@users.noreply.github.com>
2018-12-14 14:18:46 -0500

    Update project3.s
    improving formatting and readability

--- project3.s
+++ project3.s
@@ -1,4 +1,4 @@
-.data		
+..data		
 #Invalid Messages
 char_array:				.space 4000
 string_is_too_long:			.asciiz "Input is too long."
@@ -12,23 +12,23 @@ main:
 		li $v0, 8
 		la $a0, char_array
 		syscall
-		li $t0, 32				#storing space into $t3
-		li $t1, 0				#counter i = 0					
-		li $s0, 0				#counter to help keep track of previous character. initialized as 0
-		la $t3, char_array			#loading userInput address into register
-		li $t4, 0				#number of characters = 0
-		li $t5, 10				#loaded new line into $t5
-		li $t6, 0				#second counter to track number of spaces before actual input
+		li $t0, 32					#storing space into $t3
+		li $t1, 0					#counter i = 0					
+		li $s0, 0					#counter to help keep track of previous character. initialized as 0
+		la $t3, char_array				#loading userInput address into register
+		li $t4, 0					#number of characters = 0
+		li $t5, 10					#loaded new line into $t5
+		li $t6, 0					#second counter to track number of spaces before actual input
 
 loop:
-		lb $t7, 0($t3)				#get string character
-		beq $t7, $t5, break_loop		#break if character is a newline char
+		lb $t7, 0($t3)					#get string input
+		beq $t7, $t5, break_loop			#break if character is a newline char
 	#branch instructions for different conditions
-		beq $t7, $t0, skip_invalid_spaces         #if character is not a space and
-		bne $s0, $t0, skip_invalid_spaces         #if the previous character is a space &
-		beq $t4, $0, skip_invalid_spaces          #if the number of previously seen characters is not zero and
-		beq $t7, $0, skip_invalid_spaces          #if the character is not null and
-		beq $t7, $t5, skip_invalid_spaces         #if the character is not new line then print invalid
+		beq $t7, $t0, skip_invalid_spaces         #if mycharacter is_not_a_space &
+		bne $s0, $t0, skip_invalid_spaces         #if the previous_checked_character is a space &
+		beq $t4, $0, skip_invalid_spaces          #if the number_of_previously_checked chars is not 0 &
+		beq $t7, $0, skip_invalid_spaces          #character is not null &
+		beq $t7, $t5, skip_invalid_spaces         #the character is not a new line then proceed else skip to skip_invalid_spaces label
 	
         li $v0, 4
         la $a0, not_valid
